1328355da933279f3e3ea8bac625a07402500a1b
DevPrinceK <princesamuelpks@gmail.com>
2022-03-09 23:44:44 +0000

    fix realted name conflicts

--- core/dashboard/models.py
+++ core/dashboard/models.py
@@ -5,9 +5,9 @@ from accounts.models import Account
 
 class Service(models.Model):
     customer = models.ForeignKey(
-        Account, on_delete=models.SET_NULL, null=True, related_name='customer')
+        Account, on_delete=models.CASCADE, null=True, related_name='requested')
     labourer = models.ForeignKey(
-        Account, on_delete=models.SET_NULL, null=True, related_name='labourer')
+        Account, on_delete=models.CASCADE, null=True, related_name='doer')
     service_name = models.CharField(max_length=200)
     service_description = models.TextField(null=True, blank=True)
     charge = models.DecimalField(max_digits=10, decimal_places=3)
@@ -28,8 +28,10 @@ class Product(models.Model):
     name = models.CharField(verbose_name="Product Name", max_length=100)
     image = models.ImageField(
         verbose_name="Product Image", upload_to="product/", null=True, blank=True)
-    min_price = models.DecimalField(verbose_name="Minimum Price", )
-    max_price = models.DecimalField(verbose_name="Maximum Price")
+    min_price = models.DecimalField(
+        verbose_name="Minimum Price", decimal_places=3, max_digits=10)
+    max_price = models.DecimalField(
+        verbose_name="Maximum Price", decimal_places=3, max_digits=10)
 
     def __str__(self):
         return self.name
@@ -37,8 +39,10 @@ class Product(models.Model):
 
 class Wallet(models.Model):
     wallet_id = models.CharField(verbose_name="Wallet ID", max_length=50)
-    main_balance = models.DecimalField(verbose_name="Main Balance")
-    available_balance = models.DecimalField(verbose_name="available Balance")
+    main_balance = models.DecimalField(
+        verbose_name="Main Balance", decimal_places=3, max_digits=10)
+    available_balance = models.DecimalField(
+        verbose_name="available Balance", decimal_places=3, max_digits=10)
 
     def __str__(self):
         return self.wallet_id
@@ -47,14 +51,15 @@ class Wallet(models.Model):
 class Transaction(models.Model):
     transaction_id = models.CharField(verbose_name="Wallet ID", max_length=50)
     customer = models.ForeignKey(
-        Account, on_delete=models.SET_NULL, verbose_name="Customer")
+        Account, on_delete=models.CASCADE, related_name='customer')
     labourer = models.ForeignKey(
-        Account, on_delete=models.SET_NULL, verbose_name="Labourer")
-    amount = models.DecimalField(verbose_name="Amount")
+        Account, on_delete=models.CASCADE, verbose_name="Labourer")
+    amount = models.DecimalField(
+        verbose_name="Amount", decimal_places=3, max_digits=10)
     payment_mode = models.CharField(
         verbose_name="Mode of Payment", max_length=50)
     service = models.CharField(verbose_name="Service Rendered", max_length=50)
-    wallet = models.ForeignKey(Wallet, on_delete=models.SET_NULL)
+    wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
     payment = models.CharField(verbose_name="Payment Status", max_length=50)
     payment_date = models.DateTimeField(verbose_name="Payment date")
 
