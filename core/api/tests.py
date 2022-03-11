d2e6d646409ab71affd55b1b4f16963d82dba5ac
DevPrinceK <princesamuelpks@gmail.com>
2022-03-11 10:14:14 +0000

    create more tests

--- core/api/tests.py
+++ core/api/tests.py
@@ -1,3 +1,4 @@
+from urllib import response
 from django.test import TestCase
 from django.urls import reverse
 from http import HTTPStatus
@@ -11,33 +12,66 @@ class TestAPIOverview(TestCase):
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
 
-class TestAccountList(TestCase):
+class TestAccount(TestCase):
 
     def test_account_list_returns(self):
         url = reverse('api:account_list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
+    def test_account_detail_returns(self):
+        url = reverse('api:user', args=(1,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
 
-class TestProductList(TestCase):
+
+class TestProduct(TestCase):
 
     def test_product_list_returns(self):
         url = reverse('api:product_list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
+    def test_product_detail_returns(self):
+        url = reverse('api:product', args=(1,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
 
-class TestServiceList(TestCase):
+
+class TestService(TestCase):
 
     def test_service_list_returns(self):
         url = reverse('api:service_list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
+    def test_service_detail_returns(self):
+        url = reverse('api:service', args=(1,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
 
-class TestTransactionList(TestCase):
+
+class TestTransaction(TestCase):
 
     def test_transaction_list_returns(self):
         url = reverse('api:transaction_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_transaction_detail_returns(self):
+        url = reverse('api:transaction', args=(1,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+
+class TestWallet(TestCase):
+
+    def test_wallet_list_returns(self):
+        url = reverse('api:wallet_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_wallet_detail_returns(self):
+        url = reverse('api:wallet', args=(1,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, HTTPStatus.OK)
