64e8918de8eab63d47167ef8d0fba4e479cf3bba
DevPrinceK <princesamuelpks@gmail.com>
2022-03-11 10:14:43 +0000

    update file

--- core/api/urls.py
+++ core/api/urls.py
@@ -4,13 +4,22 @@ from . import views as APIViews
 
 app_name = 'api'
 urlpatterns = [
+    path('sign-up/', APIViews.SignUp.as_view(), name='sign_up'),
+
     path('endpoints/', APIViews.APIOverView.as_view(), name='api_overview'),
+    path('accounts/', APIViews.AccountList.as_view(), name='account_list'),
     path('products/', APIViews.ProductList.as_view(), name='product_list'),
+    path('wallets/', APIViews.WalletList.as_view(), name='wallet_list'),
     path('services/', APIViews.ServiceList.as_view(), name='service_list'),
     path('transactions/', APIViews.TransactionList.as_view(),
          name='transaction_list'),
-    path('accounts/', APIViews.AccountList.as_view(), name='account_list'),
-    path('wallets/', APIViews.WalletList.as_view(), name='wallet_list'),
-    path('user/<int:pk>/', APIViews.OneUser.as_view(), name='one_user'),
-    path('sign-up/', APIViews.SignUp.as_view(), name='sign_up'),
+
+    path('user/<int:pk>/', APIViews.AccountDetail.as_view(), name='user'),
+    path('product/<int:pk>/', APIViews.ProductDetail.as_view(), name='product'),
+    path('wallet/<int:pk>/', APIViews.WalletDetail.as_view(), name='wallet'),
+    path('service/<int:pk>/', APIViews.ServiceDetail.as_view(), name='service'),
+    path('transaction/<int:pk>/',
+         APIViews.TransactionDetail.as_view(), name='transaction'),
+
+
 ]
--- core/api/views.py
+++ core/api/views.py
@@ -1,4 +1,5 @@
 import imp
+from multiprocessing import Manager
 from django.shortcuts import render
 from django.views import View
 from accounts.models import Account
@@ -8,7 +9,7 @@ from rest_framework.response import Response
 from django.utils.decorators import method_decorator
 
 
-from .serializers import AccountSerializer, WalletSerializer, TransactionSerializer, ProductSerializer, ServiceSerializer
+from .serializers import AccountSerializer, WalletSerializer, TransactionSerializer, ProductSerializer, ServiceSerializer, SignUpSerializer
 
 
 class APIOverView(View):
@@ -38,12 +39,30 @@ class AccountList(View):
         return Response(serializer.data)
 
 
+class AccountDetail(View):
+
+    @method_decorator(api_view(['GET']))
+    def get(self, request, pk, *args, **kwargs):
+        account = Account.objects.filter(id=pk).first()
+        serializer = AccountSerializer(account, many=False)
+        return Response(serializer.data)
+
+
 class ProductList(View):
 
     @method_decorator(api_view(['GET']))
     def get(self, request):
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+
+class ProductDetail(View):
+
+    @method_decorator(api_view(['GET']))
+    def get(self, request, pk, *args, **kwargs):
+        product = Product.objects.filter(id=pk).first()
+        serializer = ProductSerializer(product, many=False)
         return Response(serializer.data)
 
 
@@ -56,12 +75,30 @@ class ServiceList(View):
         return Response(serializer.data)
 
 
+class ServiceDetail(View):
+
+    @method_decorator(api_view(['GET']))
+    def get(self, request, pk, *args, **kwargs):
+        service = Service.objects.filter(id=pk).first()
+        serializer = ServiceSerializer(service, many=False)
+        return Response(serializer.data)
+
+
 class TransactionList(View):
 
     @method_decorator(api_view(['GET']))
     def get(self, request):
         transactions = Transaction.objects.all()
         serializer = TransactionSerializer(transactions, many=True)
+        return Response(serializer.data)
+
+
+class TransactionDetail(View):
+
+    @method_decorator(api_view(['GET']))
+    def get(self, request, pk, *args, **kwargs):
+        transaction = Transaction.objects.filter(id=pk).first()
+        serializer = TransactionSerializer(transaction, many=False)
         return Response(serializer.data)
 
 
@@ -74,20 +111,20 @@ class WalletList(View):
         return Response(serializer.data)
 
 
-class OneUser(View):
+class WalletDetail(View):
 
-    @method_decorator(api_view(['GET']))
-    def get(self, request, pk):
-        user = Account.objects.filter(id=pk).first()
-        serializer = AccountSerializer(user)
+    @method_decorator(api_view())
+    def get(self, request, pk, *args, **kwargs):
+        wallet = Wallet.objects.filter(id=pk).first()
+        serializer = WalletSerializer(wallet, many=False)
         return Response(serializer.data)
 
 
 class SignUp(View):
 
-    @method_decorator(api_view(['POST']))
+    @method_decorator(api_view(['GET', 'POST']))
     def post(self, request):
-        serializer = AccountSerializer(data=request.data)
+        serializer = SignUpSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
