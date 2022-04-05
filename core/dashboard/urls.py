e5ab35812cd6d1546918c1a52105c15f8c8e52c2
DevPrinceK <princesamuelpks@gmail.com>
2022-04-05 17:53:40 +0000

    Update urls.py
    transaction and payment urls

--- core/dashboard/urls.py
+++ core/dashboard/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'dashboard'
 urlpatterns = [
     path('dashboard/', views.DashboardView.as_view(), name='dashboard'),
+    path('transactions/', views.TransactionListView.as_view(), name='transactions'),
+    path('make-payment/', views.MakePaymentView.as_view(), name='make_payment'),
 ]
