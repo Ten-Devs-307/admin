5436a6826f2973df2e22f12e6d2f2978745b1688
DevPrinceK <princesamuelpks@gmail.com>
2022-04-18 13:41:54 +0000

    Update views.py
    Customer List CBV
    Make Payment CBV
    Receive Payment CBV
    Admin List CBV
    Labourer List CBV

--- core/dashboard/views.py
+++ core/dashboard/views.py
@@ -5,17 +5,19 @@ from datetime import datetime, timedelta
 from django.shortcuts import render, redirect
 from django.views import View
 from django.utils.decorators import method_decorator
+from accounts.models import Account
 
 from core import settings
-from core.util.util_functions import get_transaction_status, make_payment
+from core.util.decorators import AdminsOnly
+from core.util.util_functions import get_transaction_status, make_payment, receive_payment
 from .models import Product, Service, Transaction, Wallet
-from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 
 class DashboardView(View):
     template_name = 'dashboard/dashboard.html'
 
+    @method_decorator(AdminsOnly)
     def get(self, request, *args, **kwargs):
         num_of_services = Service.objects.count()
         num_of_services_this_week = Service.objects.filter(
@@ -46,6 +48,7 @@ class DashboardView(View):
 class TransactionListView(View):
     template_name = 'dashboard/transactions.html'
 
+    @method_decorator(AdminsOnly)
     def get(self, request, *args, **kwargs):
         query = request.GET.get('q')
         if query:
@@ -59,18 +62,123 @@ class TransactionListView(View):
         return render(request, self.template_name, context)
 
 
+class CustomerListView(View):
+    template_name = 'dashboard/customer_list.html'
+
+    @method_decorator(AdminsOnly)
+    def get(self, request, *args, **kwargs):
+        customers = Account.objects.filter(is_customer=True).order_by('-id')
+        context = {
+            'customers': customers
+        }
+        return render(request, self.template_name, context)
+
+
+class LabourerListView(View):
+    template_name = 'dashboard/labourer_list.html'
+
+    @method_decorator(AdminsOnly)
+    def get(self, request, *args, **kwargs):
+        labourers = Account.objects.filter(is_labourer=True).order_by('-id')
+        context = {
+            'labourers': labourers,
+        }
+        return render(request, self.template_name, context)
+
+
+class LabourerDetailsView(View):
+    template_name = 'dashboard/labourer_details.html'
+
+    @method_decorator(AdminsOnly)
+    def get(self, request, *args, **kwargs):
+        labourer_id = request.GET.get('labourer_id')
+        labourer = Account.objects.get(id=labourer_id)
+        context = {
+            'labourer': labourer,
+        }
+        return render(request, self.template_name, context)
+
+
+class AdminListView(View):
+    template_name = 'dashboard/admin_list.html'
+
+    @method_decorator(AdminsOnly)
+    def get(self, request, *args, **kwargs):
+        admins = Account.objects.filter(
+            Q(is_superuser=True) | Q(is_staff=True)
+        ).order_by('-id')
+        context = {
+            'admins': admins,
+        }
+        return render(request, self.template_name, context)
+
+
+class ReceivePaymentView(View):
+    template_name = 'dashboard/receive_payment.html'
+
+    def generate_transaction_id(self):
+        return int(round(time.time() * 1000))
+
+    @ method_decorator(AdminsOnly)
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return render(request, self.template_name, context)
+
+    @ method_decorator(AdminsOnly)
+    def post(self, request, *args, **kwargs):
+        print('the payment is being processed')
+        user = request.user
+        phone = request.POST.get('phone')
+        amount = request.POST.get('amount')
+        network = request.POST.get('network')
+        note = request.POST.get('note')
+        transaction_id = self.generate_transaction_id()
+
+        data = {
+            'transaction_id': transaction_id,
+            'mobile_number': phone,
+            'amount': amount,
+            'wallet_id': settings.WALLET_ID,
+            'network_code': network,
+            'note': note,
+        }
+        response = receive_payment(data)
+        # wait for 30 seconds for transaction to be processed
+        for i in range(3):
+            time.sleep(10)
+            transaction_status = get_transaction_status(transaction_id)
+            if transaction_status['success'] == True:
+                print('the transaction was successful')
+                # user.has_paid = True
+                break
+
+        transaction = {
+            'transaction_id': transaction_id,
+            'amount': amount,
+            'from_phone': phone,
+            'network': network,
+            'note': note,
+            'payment_status_code': transaction_status['status_code'],
+            'payment_status': transaction_status['message'],
+            'customer': user,
+            'labourer': user,
+        }
+        Transaction.objects.create(**transaction)
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
 class MakePaymentView(View):
     template_name = 'dashboard/make_payment.html'
 
     def generate_transaction_id(self):
         return int(round(time.time() * 1000))
 
-    # @method_decorator(ApplicantsOnly)
+    @ method_decorator(AdminsOnly)
     def get(self, request, *args, **kwargs):
         context = {}
         return render(request, self.template_name, context)
 
-    # @method_decorator(ApplicantsOnly)
+    @ method_decorator(AdminsOnly)
     def post(self, request, *args, **kwargs):
         print('the payment is being processed')
         user = request.user
@@ -110,5 +218,4 @@ class MakePaymentView(View):
             'labourer': user,
         }
         Transaction.objects.create(**transaction)
-        # return redirect('applicant:transactions')
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
