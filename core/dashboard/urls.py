1bcec8821df8ae6f86b57b9c12967efedd3005c5
DevPrinceK <princesamuelpks@gmail.com>
2022-04-18 13:40:55 +0000

    Update urls.py

--- core/dashboard/urls.py
+++ core/dashboard/urls.py
@@ -5,5 +5,13 @@ app_name = 'dashboard'
 urlpatterns = [
     path('dashboard/', views.DashboardView.as_view(), name='dashboard'),
     path('transactions/', views.TransactionListView.as_view(), name='transactions'),
+    path('receive-payment/', views.ReceivePaymentView.as_view(),
+         name='receive_payment'),
     path('make-payment/', views.MakePaymentView.as_view(), name='make_payment'),
+
+    path('customers/', views.CustomerListView.as_view(), name='customers'),
+    path('labourers/', views.LabourerListView.as_view(), name='labourers'),
+    path('labourer-detail/<str:labourer_id>/',
+         views.LabourerDetailsView.as_view(), name='labourer_details'),
+    path('administrators/', views.AdminListView.as_view(), name='admins'),
 ]
