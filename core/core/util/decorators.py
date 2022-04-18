6dfa91de3fa1520505ec55311db5e363352f19b7
DevPrinceK <princesamuelpks@gmail.com>
2022-04-18 13:27:17 +0000

    Create decorators.py
    Admins only login required decorator

---
+++ core/core/util/decorators.py
@@ -0,0 +1,17 @@
+from django.shortcuts import redirect, HttpResponse
+from django.contrib import messages
+
+
+class AdminsOnly(object):
+    def __init__(self, function):
+        self.function = function
+
+    def __call__(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            if request.user.is_superuser or request.user.is_staff:
+                return self.function(request, *args, **kwargs)
+            else:
+                return HttpResponse("You are not authorized to view this page")
+        else:
+            messages.error(request, "You must login to continue!")
+            return redirect('accounts:login')
