38970c0cf59bf9a4bf49db8a3a1cc2246474e8f1
DevPrinceK <princesamuelpks@gmail.com>
2022-04-18 13:38:18 +0000

    Create signals.py
    signal to create wallet afor labourers and admins

---
+++ core/dashboard/signals.py
@@ -0,0 +1,26 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+
+from accounts.models import Account
+from dashboard.models import Wallet
+
+
+@receiver(post_save, sender=Account)
+def create_wallet(sender, instance, created, **kwargs):
+    '''Create a wallet for each new staff member. Also create a wallet for each approved labourer'''
+    if created == True:
+        if instance.is_staff or instance.is_superuser:
+            wallet = Wallet.objects.create(holder=instance)
+            wallet.save()
+            instance.wallet = wallet
+            instance.save()
+            print('Wallet created for staff member')
+    else:
+        if instance.is_labourer or instance.is_staff or instance.is_superuser:
+            if not instance.wallet:
+                wallet = Wallet.objects.create(holder=instance)
+                wallet.save()
+                instance.wallet = wallet
+                instance.save()
+                print('Wallet created for labourer')
