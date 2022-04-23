a2fbbd9c9be8f0339de93d406d9d788efb7b163b
DevPrinceK <princesamuelpks@gmail.com>
2022-04-23 20:57:09 +0000

    remove name parameter from account manager

--- core/accounts/manager.py
+++ core/accounts/manager.py
@@ -4,14 +4,14 @@ from django.contrib.auth.models import BaseUserManager
 class AccountManager(BaseUserManager):
     '''Managers user / voter accounts'''
 
-    def create_user(self, email, name, password, **kwargs):
-        user = self.model(email=email, name=name, password=password, **kwargs)
+    def create_user(self, email, password, **kwargs):
+        user = self.model(email=email, password=password, **kwargs)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, email, name, password, **kwargs):
-        user = self.create_user(email, name,  password, **kwargs)
+    def create_superuser(self, email, password, **kwargs):
+        user = self.create_user(email,  password, **kwargs)
         user.is_superuser = True
         user.is_staff = True
         user.save()
