5ecf924f34cc81df26156517b0a23f7f31217f94
DevPrinceK <princesamuelpks@gmail.com>
2022-04-24 15:39:56 +0000

    Create chartjs.js
    implement metric charts for dashboard

---
+++ core/dashboard/static/assets/js/chartjs.js
@@ -0,0 +1,442 @@
+$(function () {
+	'use strict';
+	var ctx1 = document.getElementById('chartBar1').getContext('2d');
+	new Chart(ctx1, {
+		type: 'bar',
+		data: {
+			labels: ['Prince', 'Adwoa', 'Joe', 'Angela', 'Emma', 'Ofori'],
+			datasets: [{
+				label: 'Rating',
+				data: [12, 39, 20, 10, 25, 18],
+				backgroundColor: '#285cf7'
+			}]
+		},
+		options: {
+			maintainAspectRatio: false,
+			responsive: true,
+			legend: {
+				display: false,
+				labels: {
+					display: false
+				}
+			},
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero: true,
+						fontSize: 10,
+						max: 80,
+						fontColor: "rgba(171, 167, 167,0.9)",
+					},
+					gridLines: {
+						display: true,
+						color: 'rgba(171, 167, 167,0.2)',
+						drawBorder: false
+					},
+				}],
+				xAxes: [{
+					barPercentage: 0.6,
+					ticks: {
+						beginAtZero: true,
+						fontSize: 11,
+						fontColor: "rgba(171, 167, 167,0.9)",
+					},
+					gridLines: {
+						display: true,
+						color: 'rgba(171, 167, 167,0.2)',
+						drawBorder: false
+					},
+				}]
+			}
+		}
+	});
+	var ctx2 = document.getElementById('chartBar2').getContext('2d');
+	new Chart(ctx2, {
+		type: 'bar',
+		data: {
+			labels: ['Prince', 'Adwoa', 'Joe', 'Angela', 'Emma', 'Ofori'],
+			datasets: [{
+				label: 'Current Balance',
+				data: [12, 39, 20, 10, 25, 18],
+				backgroundColor: 'rgba(0,123,255,.5)'
+			}]
+		},
+		options: {
+			maintainAspectRatio: false,
+			responsive: true,
+			legend: {
+				display: false,
+				labels: {
+					display: false
+				}
+			},
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero: true,
+						fontSize: 10,
+						max: 80,
+						fontColor: "rgba(171, 167, 167,0.9)",
+					},
+					gridLines: {
+						display: true,
+						color: 'rgba(171, 167, 167,0.2)',
+						drawBorder: false
+					},
+				}],
+				xAxes: [{
+					barPercentage: 0.6,
+					ticks: {
+						beginAtZero: true,
+						fontSize: 11,
+						fontColor: "rgba(171, 167, 167,0.9)",
+					},
+					gridLines: {
+						display: true,
+						color: 'rgba(171, 167, 167,0.2)',
+						drawBorder: false
+					},
+				}]
+			}
+		}
+	});
+
+
+	var ctx3 = document.getElementById('chartBar3').getContext('2d');
+	var gradient = ctx3.createLinearGradient(0, 0, 0, 250);
+	gradient.addColorStop(0, '#285cf7');
+	gradient.addColorStop(1, '#f7557a');
+	new Chart(ctx3, {
+		type: 'bar',
+		data: {
+			labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+			datasets: [{
+				label: 'Current Balance',
+				data: [12, 39, 20, 10, 25, 18],
+				backgroundColor: gradient
+			}]
+		},
+		options: {
+			maintainAspectRatio: false,
+			responsive: true,
+			legend: {
+				display: false,
+				labels: {
+					display: false
+				}
+			},
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero: true,
+						fontSize: 10,
+						max: 80,
+						fontColor: "rgba(171, 167, 167,0.9)",
+					},
+					gridLines: {
+						display: true,
+						color: 'rgba(171, 167, 167,0.2)',
+						drawBorder: false
+					},
+				}],
+				xAxes: [{
+					barPercentage: 0.6,
+					ticks: {
+						beginAtZero: true,
+						fontSize: 11,
+						fontColor: "rgba(171, 167, 167,0.9)",
+					},
+					gridLines: {
+						display: true,
+						color: 'rgba(171, 167, 167,0.2)',
+						drawBorder: false
+					},
+				}]
+			}
+		}
+	});
+
+	/** STACKED BAR CHART **/
+	/** Chart for daily services rendered: Monday - Sunday **/
+	var ctx6 = document.getElementById('chartStacked1');
+	new Chart(ctx6, {
+		type: 'bar',
+		data: {
+			labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+			datasets: [{
+				data: [10, 24, 20, 25, 35, 50, 45],
+				backgroundColor: '#007bff',
+				borderWidth: 1,
+				fill: true
+			}, {
+				data: [10, 24, 20, 25, 35, 50, 30],
+				backgroundColor: '#58a2f1',
+				borderWidth: 1,
+				fill: true
+			}, {
+				data: [20, 30, 28, 33, 45, 65, 56],
+				backgroundColor: '#c9e1fb',
+				borderWidth: 1,
+				fill: true
+			}]
+		},
+		options: {
+			maintainAspectRatio: false,
+			legend: {
+				display: false,
+				labels: {
+					display: false
+				}
+			},
+			scales: {
+				yAxes: [{
+					stacked: true,
+					ticks: {
+						beginAtZero: true,
+						fontSize: 11,
+						fontColor: "rgba(171, 167, 167,0.9)",
+					},
+					gridLines: {
+						display: true,
+						color: 'rgba(171, 167, 167,0.2)',
+						drawBorder: false
+					},
+				}],
+				xAxes: [{
+					barPercentage: 0.5,
+					stacked: true,
+					ticks: {
+						fontSize: 11,
+						fontColor: "rgba(171, 167, 167,0.9)",
+					},
+					gridLines: {
+						display: true,
+						color: 'rgba(171, 167, 167,0.2)',
+						drawBorder: false
+					},
+				}]
+			}
+		}
+	});
+
+
+	var ctx7 = document.getElementById('chartStacked2');
+	new Chart(ctx7, {
+		type: 'horizontalBar',
+		data: {
+			labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+			datasets: [{
+				data: [10, 24, 20, 25, 35, 50, 45],
+				backgroundColor: '#007bff',
+				borderWidth: 1,
+				fill: true
+			}, ]
+		},
+		options: {
+			maintainAspectRatio: false,
+			legend: {
+				display: false,
+				labels: {
+					display: false
+				}
+			},
+			scales: {
+				yAxes: [{
+					stacked: true,
+					ticks: {
+						beginAtZero: true,
+						fontSize: 10,
+						max: 80,
+						fontColor: "rgba(171, 167, 167,0.9)",
+					},
+					gridLines: {
+						display: true,
+						color: 'rgba(171, 167, 167,0.2)',
+						drawBorder: false
+					},
+				}],
+				xAxes: [{
+					stacked: true,
+					ticks: {
+						beginAtZero: true,
+						fontSize: 11,
+						fontColor: "rgb(171, 167, 167,0.9)",
+					},
+					gridLines: {
+						display: true,
+						color: 'rgba(171, 167, 167,0.2)',
+						drawBorder: false
+					},
+				}]
+			}
+		}
+	});
+
+	/* LINE CHART */
+	/** Transactions last week - Monday - Sunday **/
+	/** Transactions this week - Monday - Sunday **/
+	var ctx8 = document.getElementById('chartLine1');
+	new Chart(ctx8, {
+		type: 'line',
+		data: {
+			labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+			datasets: [{
+				data: [12, 15, 18, 40, 35, 38, 32],
+				borderColor: '#f7557a ',
+				borderWidth: 1,
+				fill: false
+			}, {
+				data: [10, 20, 25, 55, 50, 45, 35],
+				borderColor: '#007bff',
+				borderWidth: 1,
+				fill: false
+			}]
+		},
+		options: {
+			maintainAspectRatio: false,
+			legend: {
+				display: false,
+				labels: {
+					display: false
+				}
+			},
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero: true,
+						fontSize: 10,
+						max: 80,
+						fontColor: "rgba(171, 167, 167,0.9)",
+					},
+					gridLines: {
+						display: true,
+						color: 'rgba(171, 167, 167,0.2)',
+						drawBorder: false
+					},
+				}],
+				xAxes: [{
+					ticks: {
+						beginAtZero: true,
+						fontSize: 11,
+						fontColor: "rgba(171, 167, 167,0.9)",
+					},
+					gridLines: {
+						display: true,
+						color: 'rgba(171, 167, 167,0.2)',
+						drawBorder: false
+					},
+				}]
+			}
+		}
+	});
+
+
+	/** AREA CHART **/
+	/** Disbursements last week - Monday - Sunday **/
+	/** Disbursements this week - Monday - Sunday **/
+	var ctx9 = document.getElementById('chartArea1');
+	var gradient1 = ctx3.createLinearGradient(0, 350, 0, 0);
+	gradient1.addColorStop(0, 'rgba(247, 85, 122,0)');
+	gradient1.addColorStop(1, 'rgba(247, 85, 122,.5)');
+	var gradient2 = ctx3.createLinearGradient(0, 280, 0, 0);
+	gradient2.addColorStop(0, 'rgba(0,123,255,0)');
+	gradient2.addColorStop(1, 'rgba(0,123,255,.3)');
+	new Chart(ctx9, {
+		type: 'line',
+		data: {
+			labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+			datasets: [{
+				data: [12, 15, 18, 40, 35, 38, 32],
+				borderColor: '#f7557a',
+				borderWidth: 1,
+				backgroundColor: gradient1
+			}, {
+				data: [10, 20, 25, 55, 50, 45, 35],
+				borderColor: '#007bff',
+				borderWidth: 1,
+				backgroundColor: gradient2
+			}]
+		},
+		options: {
+			maintainAspectRatio: false,
+			legend: {
+				display: false,
+				labels: {
+					display: false
+				}
+			},
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero: true,
+						fontSize: 10,
+						max: 80,
+						fontColor: "rgba(171, 167, 167,0.9)",
+					},
+					gridLines: {
+						display: true,
+						color: 'rgba(171, 167, 167,0.2)',
+						drawBorder: false
+					},
+				}],
+				xAxes: [{
+					ticks: {
+						beginAtZero: true,
+						fontSize: 11,
+						fontColor: "rgba(171, 167, 167,0.9)",
+					},
+					gridLines: {
+						display: true,
+						color: 'rgba(171, 167, 167,0.2)',
+						drawBorder: false
+					},
+				}]
+			}
+		}
+	});
+
+
+	/** PIE CHART **/
+	/** Chart for account holder - users of the system **/
+	var datapie = {
+		labels: ['Customers', 'Labourers', 'Staff'],
+		datasets: [{
+			data: [250, 50, 25],
+			backgroundColor: ['#285cf7', '#8500ff', '#74de00']
+		}]
+	};
+	var optionpie = {
+		maintainAspectRatio: false,
+		responsive: true,
+		legend: {
+			display: false,
+		},
+		animation: {
+			animateScale: true,
+			animateRotate: true
+		}
+	};
+
+
+	// For a doughnut chart
+	var ctx6 = document.getElementById('chartPie');
+	var myPieChart6 = new Chart(ctx6, {
+		type: 'doughnut',
+		data: datapie,
+		options: optionpie
+	});
+
+	// For a pie chart
+	var ctx7 = document.getElementById('chartDonut');
+	var myPieChart7 = new Chart(ctx7, {
+		type: 'pie',
+		data: {
+			labels: ['Customers', 'Labourers', 'Staff'],
+			datasets: [{
+				data: [250, 50, 100],
+				backgroundColor: ['#285cf7', '#8500ff', '#74de00']
+			}]
+		},
+		options: optionpie
+	});
+});
