667fd8a46e0b0bc2978b5a3fc258fec8b9eebfa8
DevPrinceK <princesamuelpks@gmail.com>
2022-04-24 17:46:07 +0000

    Update constants.py

--- core/core/util/constants.py
+++ core/core/util/constants.py
@@ -21,3 +21,9 @@ class Disbursement(Enum):
 class Reason(Enum):
     INSUFFICIENT_BALANCE = 'insufficient balance'
     CASHOUT_SUCCESSFUL = 'cashout successful'
+    CASHOUT_FAILED = 'cashout failed'
+    CREDIT_SUCCESSFUL = 'credit successful'
+    CREDIT_FAILED = 'credit failed'
+    DEBIT_SUCCESSFUL = 'debit successful'
+    DEBIT_FAILED = 'debit failed'
+    TRANSACT
