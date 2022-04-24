4b898cea00e9c8dbd02b995e1ec3f17ff1944da8
DevPrinceK <princesamuelpks@gmail.com>
2022-04-24 15:38:02 +0000

    Update constants.py
    add more constants

--- core/core/util/constants.py
+++ core/core/util/constants.py
@@ -5,8 +5,19 @@ class Status(Enum):
     APPROVED = 'approved'
     PENDING = 'pending'
     REJECTED = 'rejected'
+    PROCESSING = 'processing'
+    COMPLETED = 'completed'
+    CANCELLED = 'cancelled'
+    SUCCESSFUL = 'successful'
+    FAILED = 'failed'
 
 
 class Disbursement(Enum):
     CREDIT = 'credit'
     DEBIT = 'debit'
+    CASHOUT = 'cashout'
+
+
+class Reason(Enum):
+    INSUFFICIENT_BALANCE = 'insufficient balance'
+    CASHOUT_SUCCESSFUL = 'cashout successful'
